6d39816d6ad1b25a0a05701abfcbe2e00a499d65
Kuldeep Gohel <kuldeep.gohel@injala.com>
2025-04-13 15:19:33 +0530

    updated configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,14 @@
+// filepath: c:\Users\KuldeepGohel\demoProjects\profile-showcase-website\vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from 'path';
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: '[name].[hash][extname]',
-        chunkFileNames: '[name].[hash].js',
-        entryFileNames: '[name].[hash].js',
-      },
-    },
-  },
+  base: "/resume/", // Ensure this matches your repository name
 });
-
