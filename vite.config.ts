5ff91680400925fa182220d0eff05f79403deb8a
Kuldeep Gohel <kuldeep.gohel@injala.com>
2025-04-13 13:20:11 +0530

    added relatice paths

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,14 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name].[hash][extname]',
+        chunkFileNames: '[name].[hash].js',
+        entryFileNames: '[name].[hash].js',
+      },
+    },
+  },
 });
 
