95a495a3f2e0009976c68e0940ae7aaa1e4f954f
Kuldeep Gohel <kuldeep.gohel@injala.com>
2025-04-13 12:59:12 +0530

    added path

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/virtual-resume/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
